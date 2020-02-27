4d9e52ba623997d117b28605c2bec4768b3fa794
iamnotrodger <rodger.retanal24@gmail.com>
2020-02-26 21:34:40 -0500

    small layout change

--- airbnb/src/component/PropertyPage/PropertyPage.js
+++ airbnb/src/component/PropertyPage/PropertyPage.js
@@ -50,52 +50,59 @@ class PropertyPage extends Component {
 		const { room, reviews } = this.state;
 		return (
 			<div className='propertyPage'>
-				<div>
-					<h2> {property.title} </h2>
-					<p> {property.location} </p>
-					<p>Host</p>
-				</div>
-				<div className='roomContainer'>
-					<p>{`${room.numGuest} guests`}</p>
-					<p>{`${room.numBedroom} bedroom`}</p>
-					<p>{`${room.numBed} bed`}</p>
-					<p>{`${room.numBath} bathroom`}</p>
-				</div>
+				<div className='propertyContent'>
+					<div>
+						<h2> {property.title} </h2>
+						<p> {property.location} </p>
+						<p>Host</p>
+					</div>
+					<div className='roomContainer'>
+						<p>{`${room.numGuest} guests`}</p>
+						<p>{`${room.numBedroom} bedroom`}</p>
+						<p>{`${room.numBed} bed`}</p>
+						<p>{`${room.numBath} bathroom`}</p>
+					</div>
 
-				<div>
+					<div>
+						<div className='lineMargin'>
+							<div className='lml'></div>
+						</div>
+					</div>
+					<div>
+						<p>
+							Id dolor do mollit Lorem
+							dolore adipisicing.
+							Aliquip sint esse mollit
+							eu ad. Esse duis sit
+							occaecat ullamco eu aute
+							in adipisicing
+							exercitation dolor
+							aliquip adipisicing
+							culpa. Ea duis nisi
+							eiusmod tempor aliqua
+							commodo pariatur
+							exercitation non
+							adipisicing pariatur in
+							duis commodo. Duis
+							commodo velit irure do
+							sint nostrud minim
+							deserunt consectetur
+							laboris fugiat dolore
+							aute. Velit amet dolor
+							enim non. Ea eu irure ex
+							sunt.
+						</p>
+					</div>
+
 					<div className='lineMargin'>
 						<div className='lml'></div>
 					</div>
+					<ReviewList reviews={reviews} />
 				</div>
+
 				<div>
-					<p>
-						Id dolor do mollit Lorem dolore
-						adipisicing. Aliquip sint esse
-						mollit eu ad. Esse duis sit
-						occaecat ullamco eu aute in
-						adipisicing exercitation dolor
-						aliquip adipisicing culpa. Ea
-						duis nisi eiusmod tempor aliqua
-						commodo pariatur exercitation
-						non adipisicing pariatur in duis
-						commodo. Duis commodo velit
-						irure do sint nostrud minim
-						deserunt consectetur laboris
-						fugiat dolore aute. Velit amet
-						dolor enim non. Ea eu irure ex
-						sunt.
-					</p>
-				</div>
-
-				<div className='lineMargin'>
-					<div className='lml'></div>
-				</div>
-
-				{/* <div>
 					<Booking />
-				</div> */}
-
-				<ReviewList reviews={reviews} />
+				</div>
 			</div>
 		);
 	}
