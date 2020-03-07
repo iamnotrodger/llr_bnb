9e78aabb784c6ad0bdc37034b34b87fd180bdfd2
cyclexit <cyclexit@gmail.com>
2020-03-07 15:01:17 -0500

    Successfully connect to the database

--- airbnb-api/server.js
+++ airbnb-api/server.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const cors = require('cors');
+// set up database
+const {Client} = require('pg')
 const connection = require('./connection')
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        host: connection.host,
-        user: connection.user,
-        password: connection.password,
-        databse: connection.databse
-    }
-});
-const app = express();
-
-app.listen(3000);
-app.use(express.json());
-app.use(cors());
+const db = new Client({
+    host: connection.host,
+    user: connection.user,
+    password: connection.password,
+    database: connection.database,
+    port: 15432
+})
+db.connect()
+// set up web server
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.listen(3000)
 app.get('/', function (req, res) {
     res.end('The server is running on port 3000...');
-});
-app.post('/register', function (req, res) {
-    const { email, password } = req.body;
-    res.status(200).send('OK');
-    res.end();
-    console.log(email + " " + password);
 });
