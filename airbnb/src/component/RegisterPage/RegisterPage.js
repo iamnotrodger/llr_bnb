7dd7131a3a3f57f0c7d1237101d6304f3d7974fb
Kangwei Liao <miaochips@Kangweis-MacBook-Pro.local>
2020-03-08 12:06:24 -0400

    init register page

---
+++ airbnb/src/component/RegisterPage/RegisterPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class RegisterPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default RegisterPage
