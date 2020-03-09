3b6b4bf13d58d457502f1e82ae97859eba951ee9
iamnotrodger <rodger.retanal24@gmail.com>
2020-03-09 15:06:22 -0400

    experimenting with Joi

--- airbnb-api/controller/login.js
+++ airbnb-api/controller/login.js
@@ -1,36 +1,59 @@
 // WARNING: without encryption
 // TODO: encrypt the password
-const handleLogin = async(req, res, db_pool) => {
-    // handle http request
-    const {email, password} = req.body
-    if (!email || !password) {
-        res.end()
-        return res.status(400).send('Some unacceptable empty fields')
-    }
-    const input_password = password
-    // check the email and password
-    const client = await db_pool.connect()
-    try {
-        const queryText = 'SELECT password FROM project.login WHERE email = $1'
-        client.query(
-            queryText, [email]
-        ).then(db_res => {
-            const {password} = db_res.rows[0]
-            console.log(password) // test
-            if (input_password == password) {
-                res.status(200).send('Login successfully')
-            } else {
-                res.status(400).send('Incorrect password')
-            }
-        }).catch(err => {
-            console.error("Error during the query.", err.stack)
-            res.status(400).send('No such email.')
-        })
-    } finally {
-        client.release()
-    }
-}
+const handleLogin = async (req, res, db_pool, Joi) => {
+	// handle http request
+	const schema = {
+		email: Joi.string()
+			.min(3)
+			.required(),
+		password: Joi.string()
+			.min(8)
+			.required()
+	};
+
+	const result = Joi.validate(req.body, schema);
+
+	if (result.error) {
+		// res.end();
+		// res.status(400).send(result.error.details[0].message);
+		res.status(400).send('Invalid Inputs');
+		return;
+	}
+
+	const { email, password } = req.body;
+
+	const input_password = password;
+	// check the email and password
+	const client = await db_pool.connect();
+	try {
+		const queryText =
+			'SELECT password FROM project.login WHERE email = $1';
+		client.query(queryText, [email])
+			.then(db_res => {
+				const { password } = db_res.rows[0];
+				console.log(password); // test
+				if (input_password == password) {
+					res.status(200).send(
+						'Login successfully'
+					);
+				} else {
+					res.status(400).send(
+						'Incorrect password'
+					);
+				}
+			})
+			.catch(err => {
+				console.error(
+					'Error during the query.',
+					err.stack
+				);
+				res.status(400).send('No such email.');
+			});
+	} finally {
+		client.release();
+	}
+};
 
 module.exports = {
-    handleLogin
-}
+	handleLogin
+};
--- airbnb-api/server.js
+++ airbnb-api/server.js
@@ -1,30 +1,40 @@
 /*set up database*/
-const {Pool} = require('pg')
-const connection = require('./connection')
+const { Pool } = require('pg');
+const connection = require('./connection');
 const db_pool = new Pool({
-    host: connection.host,
-    user: connection.user,
-    password: connection.password,
-    database: connection.database,
-    port: 15432
-}) // pooled database client
+	host: connection.host,
+	user: connection.user,
+	password: connection.password,
+	database: connection.database,
+	port: 15432
+}); // pooled database client
 // db_pool.connect()
 
 /*set up web server*/
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.listen(3000) // listen to port 3000
-app.get('/', function (req, res) {
-    res.end('The server is running on port 3000...');
-})
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const Joi = require('joi');
+
+const login = require('./controller/login');
+const register = require('./controller/register');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+	res.end('The server is running on port 3000...');
+});
 
 /*handle register*/
-const register = require('./controller/register')
-app.post('/register', (req, res) => register.handleRegister(req, res, db_pool))
+app.post('/api/register', (req, res) =>
+	register.handleRegister(req, res, db_pool, Joi)
+);
 
 /*handle login*/
-const login = require('./controller/login')
-app.post('/login', (req, res) => login.handleLogin(req, res, db_pool))
+app.post('/api/login', (req, res) => login.handleLogin(req, res, db_pool, Joi));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Listening to port ${port}...`);
+}); // listen to port 3000
