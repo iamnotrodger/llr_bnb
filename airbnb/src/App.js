1f2d45201e9987693bdb53e9d895e570260d0133
iamnotrodger <rodger.retanal24@gmail.com>
2020-03-08 21:33:30 -0400

    merged recent changes

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -79,8 +79,8 @@ class App extends Component {
 	componentDidMount() {
 		this.setState({
 			user: dummyUser,
-			isSignedIn: true,
-			isHost: true,
+			isSignedIn: false,
+			isHost: false,
 			apartmentProps: dummyProps,
 			houseProps: dummyProps,
 			hotelProps: dummyProps
