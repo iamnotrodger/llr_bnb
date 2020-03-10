da2523d0df879c34ad192119b750193c2ae3d23f
iamnotrodger <rodger.retanal24@gmail.com>
2020-03-09 21:41:34 -0400

    small change

--- airbnb-api/controller/register.js
+++ airbnb-api/controller/register.js
@@ -8,10 +8,10 @@ const handleRegister = async (req, res, db_pool, Joi) => {
 			.email()
 			.min(3)
 			.max(256)
-            .required(),
-        password: Joi.string()
-            .min(8)
-            .required(),
+			.required(),
+		password: Joi.string()
+			.min(8)
+			.required(),
 		firstName: Joi.string()
 			.max(35)
 			.required(),
@@ -39,55 +39,64 @@ const handleRegister = async (req, res, db_pool, Joi) => {
 		phoneNum
 	} = req.body;
 
-    // Make the transaction with the database
-    try {
-        const client = await db_pool.connect();
-        try {
-            await client.query('BEGIN');
-            // insert into user table
-            const userText =
-                'INSERT INTO project.usr(firstName, middleName, lastName, email, address, phoneNum) VALUES($1, $2, $3, $4, $5, $6) RETURNING uid;';
-            await client
-                .query(userText, [
-                    firstName,
-                    middleName,
-                    lastName,
-                    email,
-                    address,
-                    phoneNum
-                ])
-                .then(res => {
-                    // get auto-generated uid
-                    const { uid } = res.rows[0];
-                    console.log(uid); // test
-                    // insert into guest table
-                    const guestText =
-                        'INSERT INTO project.guest(uid) VALUES($1);';
-                    client.query(guestText, [uid]);
-                    // insert into login table
-                    const loginText =
-                        'INSERT INTO project.login(email, password) VALUES($1, $2);';
-                    client.query(loginText, [email, password]);
-                })
-                .catch(err => {
-                    console.error('Postgresql Error.', err.stack);
-                });
-            await client.query('COMMIT');
-            res.status(200).send('Successful registration');
-        } catch (err) {
-            console.error(
-                'Error during the transaction, ROLLBACK.',
-                err.stack
-            );
-            await client.query('ROLLBACK');
-            res.status(400).send('Error during the registration');
-        } finally {
-            client.release();
-        }
-    } catch (err) {
-        res.status(503).send('Service Unavailable')
-        console.error('Error during the connection to the database', err.stack)
-    }
+	// Make the transaction with the database
+	try {
+		const client = await db_pool.connect();
+		try {
+			await client.query('BEGIN');
+			// insert into user table
+			const userText =
+				'INSERT INTO project.usr(firstName, middleName, lastName, email, address, phoneNum) VALUES($1, $2, $3, $4, $5, $6) RETURNING uid;';
+			await client
+				.query(userText, [
+					firstName,
+					middleName,
+					lastName,
+					email,
+					address,
+					phoneNum
+				])
+				.then(res => {
+					// get auto-generated uid
+					const { uid } = res.rows[0];
+					console.log(uid); // test
+					// insert into guest table
+					const guestText =
+						'INSERT INTO project.guest(uid) VALUES($1);';
+					client.query(guestText, [uid]);
+					// insert into login table
+					const loginText =
+						'INSERT INTO project.login(email, password) VALUES($1, $2);';
+					client.query(loginText, [
+						email,
+						password
+					]);
+				})
+				.catch(err => {
+					console.error(
+						'Postgresql Error.',
+						err.stack
+					);
+				});
+			await client.query('COMMIT');
+			res.status(200).send('Successful registration');
+		} catch (err) {
+			console.error(
+				'Error during the transaction, ROLLBACK.',
+				err.stack
+			);
+			await client.query('ROLLBACK');
+			res.status(400).send('Error during the registration');
+		} finally {
+			client.release();
+		}
+	} catch (err) {
+		res.status(503).send('Service Unavailable');
+		console.error(
+			'Error during the connection to the database',
+			err.stack
+		);
+	}
 };
 
 module.exports = {
