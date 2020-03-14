fbb4b0e91a93f821565801e9cb73a173b48f6c2e
cyclexit <cyclexit@gmail.com>
2020-03-14 11:07:10 -0400

    Send back ids(uid, gid and hid) from login

--- airbnb-api/controller/login.js
+++ airbnb-api/controller/login.js
@@ -22,16 +22,45 @@ const handleLogin = async (req, res, db_pool, Joi, CryptoJS) => {
 		const client = await db_pool.connect();
 		try {
 			const queryText =
-				'SELECT password FROM project.login WHERE email = $1';
+				'SELECT password FROM project.login WHERE email = $1;';
 			const { rows } = await client.query(queryText, [email]);
 			const { password } = rows[0];
 
-			// console.log(password) // test
-			// console.log(words.toString()) // test
-
 			if (words.toString() === password) {
-				// compare the encrypted password
-				res.status(200).send('Login successfully');
+				// try to get uid, gid and hid if it exits
+				var ids = {
+					uid: null,
+					gid: null,
+					hid: null
+				}
+				try {
+					{   // get uid
+						const uidQueryText = 'SELECT uid from project.usr WHERE email = $1;';
+						const { rows } = await client.query(uidQueryText, [email]);
+						if (rows[0] != undefined) {
+							ids.uid = rows[0].uid
+						}
+					}
+					{   // get gid
+						const gidQueryText = 'SELECT gid from project.guest WHERE uid = $1;';
+						const { rows } = await client.query(gidQueryText, [ids.uid])
+						if (rows[0] != undefined) {
+							ids.gid = rows[0].gid
+						}
+					}
+					{   // get hid
+						const hidQueryText = 'SELECT hid from project.host WHERE uid = $1;';
+						const { rows } = await client.query(hidQueryText, [ids.uid])
+						// console.log(rows[0] === undefined) // test
+						if (rows[0] != undefined) {
+							ids.hid = rows[0].hid
+						}
+					}
+					res.status(200).send(ids);
+				} catch (err) {
+					console.error('Error during the query.', err.stack);
+					res.status(400).send('Something wrong.');
+				}
 			} else {
 				res.status(400).send('Incorrect password.');
 			}
