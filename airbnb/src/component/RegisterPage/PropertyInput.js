a1305f7071e516c481b1c036ac631ae0bbdbb9a2
Kangwei Liao <miaochips@Kangweis-MacBook-Pro.local>
2020-03-14 04:32:36 -0400

    amenities list updated

--- airbnb/src/component/RegisterPage/PropertyInput.js
+++ airbnb/src/component/RegisterPage/PropertyInput.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Select from 'react-select';
-import './PropertyInput.css'
+import Collapse from "@kunukn/react-collapse";
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import cx from "classnames";
+import './PropertyInput.css';
 
 const amountOptions = [
     { value: '1', label: '1' },
@@ -16,7 +21,25 @@ const propTypeOptions = [
     { value: 'Apartment', label: 'Apartment' }
 ];
 
+// function CheckboxLabels() {
+//     const [state, setState] = React.useState({
+//       checkedA: true,
+//       checkedB: true,
+//       checkedF: true,
+//       checkedG: true,
+//     });
+// }
+
 export class PropertyInput extends Component {
+    state = {
+        listOpen1 : false
+    }
+
+    toggle = index => {
+        let collapse = "listOpen" + index;
+        this.setState(prevState => ({[collapse]: !prevState[collapse]}));
+    };
+
     render() {
         return (
             <div>
@@ -50,6 +73,58 @@ export class PropertyInput extends Component {
                         className='select'
                         placeholder='Select...'
                         options={amountOptions}
+                    />
+                </div>
+                <div className='list-amenity'>
+                    <button
+                        className={cx("list-btn", {
+                            "list-btn-active": this.state.listOpen1
+                        })}
+                        onClick={() => this.toggle(1)}
+                    >
+                        choose your amenities
+                    </button>
+                    <Collapse
+                        isOpen={this.state.listOpen1}
+                        className={
+                            "list-collapse list-collapse-gradient " +
+                            (this.state.listOpen1 ? "list-collapse-active" : "")
+                        }
+                        transition="height 800ms cubic-bezier(0.4, 0, 0.2, 1)"
+                        aria-hidden={this.state.listOpen1 ? "false" : "true"}
+                        elementType="checkbox"
+                        render={collapseState => (
+                            <React.Fragment>
+                                <div className="list-content">
+                                    <FormGroup col>
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox defaultChecked value='Essentials'/>
+                                            }
+                                            label='Essentials'
+                                        />
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox defaultChecked value='TV'/>
+                                            }
+                                            label='TV'
+                                        />
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox defaultChecked value='Wifi'/>
+                                            }
+                                            label='Wifi'
+                                        />
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox defaultChecked value='Heat'/>
+                                            }
+                                            label='Heat'
+                                        />
+                                    </FormGroup>
+                                </div>
+                            </React.Fragment>
+                        )}
                     />
                 </div>
             </div>
