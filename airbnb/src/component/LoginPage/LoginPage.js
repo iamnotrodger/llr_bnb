6ca1030dd130406051585e63df37a9f5003f26c2
iamnotrodger <rodger.retanal24@gmail.com>
2020-03-14 08:54:17 -0400

    Change login page to a function

--- airbnb/src/component/LoginPage/LoginPage.js
+++ airbnb/src/component/LoginPage/LoginPage.js
@@ -1,70 +1,72 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './LoginPage.css';
 
-class LoginPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+const LoginPage = () => {
+	const [inputValue, setInputValue] = useState({
+		firstName: '',
+		lastName: '',
+		email: ''
+	});
 
-    onChange = (event) => {
-        this.setState({[event.target.name]: event.target.value});
-        console.log(this.state.email);
-        console.log(this.state.password);
-    }
+	const onChange = event => {
+		const { name, value } = event.target;
+		setInputValue({ ...inputValue, [name]: value });
+	};
 
-    render() {
-        return (
-            <div className = 'login-page'>
-                <div className='login-box'>
-                    <p className = 'login-title'>
-                        Welcome to LLB
-                    </p>
-                    <div>
-                        <input className = 'login-input'
-                                name='email'
-                                type='email'
-                                placeholder='Email'
-                                onChange={this.onChange}
-                            />
-                    </div>
-                    <div>
-                        <input className = 'login-input'
-                            name='password'
-                            type='password'
-                            placeholder='Password'
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div>
-                        <Link to = '/register'>
-                            <p className="login-register-hint">Don't have an account?</p>
-                        </Link>
-                    </div>
-                    
-                    <div>
-                        <button
-                            type='submit'
-                            className='submitButton'
-                        >
-                            Login
-                        </button>
-                    </div>
-                </div>
-                <div className='login-background'>
-                    <img
-                        className='login-img'
-                        alt='background'
-                        src='https://images.squarespace-cdn.com/content/v1/5ae09b9cee17598796bf0561/1550011924213-C1XWZQK9HYNNPUKCV2HW/ke17ZwdGBToddI8pDm48kFrPwftsEnVJnMrFLVbMqtJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0gycmlYrUNOm5FlGNDjMZJivtP_8QmpAdw116-3ob1oTjc7unw2RKrjuCiMWmcwGEQ/image-asset.jpeg?format=2500w'
-                    />
-                </div>
-            </div>
-        );
-    }
-}
+	const handleButtonSubmit = () => {
+		console.log('hit that login');
+	};
+
+	return (
+		<div className='login-page'>
+			<div className='login-box'>
+				<p className='login-title'>Welcome to LLB</p>
+				<div>
+					<input
+						className='login-input'
+						name='email'
+						type='email'
+						placeholder='Email'
+						onChange={onChange}
+					/>
+				</div>
+				<div>
+					<input
+						className='login-input'
+						name='password'
+						type='password'
+						placeholder='Password'
+						onChange={onChange}
+					/>
+				</div>
+				<div>
+					<Link to='/register'>
+						<p className='login-register-hint'>
+							Don't have an account?
+						</p>
+					</Link>
+				</div>
+
+				<div>
+					<button
+						type='submit'
+						className='submitButton'
+						onClick={handleButtonSubmit}
+					>
+						Login
+					</button>
+				</div>
+			</div>
+			<div className='login-background'>
+				<img
+					className='login-img'
+					alt='background'
+					src='https://images.squarespace-cdn.com/content/v1/5ae09b9cee17598796bf0561/1550011924213-C1XWZQK9HYNNPUKCV2HW/ke17ZwdGBToddI8pDm48kFrPwftsEnVJnMrFLVbMqtJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0gycmlYrUNOm5FlGNDjMZJivtP_8QmpAdw116-3ob1oTjc7unw2RKrjuCiMWmcwGEQ/image-asset.jpeg?format=2500w'
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default LoginPage;
