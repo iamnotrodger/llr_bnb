8be3c85423ef07de652065a2fad72d5204b6890f
cyclexit <cyclexit@gmail.com>
2020-03-26 18:25:51 -0400

    Now create a pending payment when a rental agreement is added.

--- airbnb-api/controller/Rental/rental.js
+++ airbnb-api/controller/Rental/rental.js
@@ -19,21 +19,36 @@ const handleAddRentalAgreement = async (req, res, db_pool, Joi) => {
         return;
     }
     const { gid, prid, start_date, end_date } = req.body;
-    console.log(gid, prid, start_date, end_date); // test
+    // console.log(gid, prid, start_date, end_date); // test
     
     try {
         const client = await db_pool.connect();
         try {
+            await client.query('BEGIN');
+            // get hid
             const propertyQueryText = 
-            'SELECT hid FROM project.property WHERE prid = $1;';
+                'SELECT hid FROM project.property WHERE prid = $1;';
             const res1 = await client.query(propertyQueryText, [prid]);
-            const {hid} = res1.rows[0];
+            const { hid } = res1.rows[0];
+            // insert into rental_agreement table and get rtid
             const rentalAgreementQueryText = 
-            'INSERT INTO project.rental_agreement(gid, hid, prid, signing, start_date, end_date) VALUES($1, $2, $3, $4, $5, $6);';
-            await client.query(rentalAgreementQueryText, [gid, hid, prid, 'pending', start_date, end_date]);
+                'INSERT INTO project.rental_agreement(gid, hid, prid, signing, start_date, end_date) VALUES($1, $2, $3, $4, $5, $6) RETURNING rtid;';
+            const res2 = await client.query(rentalAgreementQueryText, [gid, hid, prid, 'pending', start_date, end_date]);
+            const { rtid } = res2.rows[0];
+            // get the price for the property
+            const pricingQueryText = 
+                'SELECT price FROM project.pricing WHERE prid = $1;';
+            const res3 = await client.query(pricingQueryText, [prid]);
+            const { price } = res3.rows[0];
+            // create a pending payment for the rental agreement
+            const paymentQueryText = 
+                'INSERT INTO project.payment(rtid, status, amount) VALUES($1, $2, $3)';
+            await client.query(paymentQueryText, [rtid, 'pending', price]);
+            await client.query('COMMIT');
             res.status(200).json('The rental agreement is added');
         } catch (err) {
-            console.error('Error during the query.', err.stack);
+            console.error('Error during the transaction, ROLLBACK.', err.stack);
+            await client.query('ROLLBACK');
             res.status(400).json('Unable to add the rental agreement.');
         } finally {
             client.release();
@@ -47,7 +62,7 @@ const handleAddRentalAgreement = async (req, res, db_pool, Joi) => {
     }
 }
 
-// Route (POST): '/api/rental/rental-agreement/host/:hid
+// Route (POST): /api/rental/rental-agreement/host/:hid
 const handleApproval = async (req, res, db_pool, Joi) => {
     // handle http request
     const schema = {
