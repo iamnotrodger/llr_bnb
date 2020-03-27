24edfb9526183668b1bbcb098bb6f4878967a6be
cyclexit <cyclexit@gmail.com>
2020-03-26 22:51:12 -0400

    Get the payment status for each rental agreement

--- airbnb-api/controller/Rental/rentalList.js
+++ airbnb-api/controller/Rental/rentalList.js
@@ -6,12 +6,20 @@ const handleHostRentalList = async (req, res, db_pool) => {
     try {
         const client = await db_pool.connect();
         try {
-            const queryText = 
+            const rentalAgreementQueryText = 
                 'SELECT * FROM project.rental_agreement WHERE hid = $1;';
-            const { rows } = await client.query(queryText, [hid]);
+            const res1 = await client.query(rentalAgreementQueryText, [hid]);
             // console.log(rows); // test
+            const paymentQueryText = 
+                'SELECT status FROM project.payment WHERE rtid = $1;';
+            for (i in res1.rows) {
+                const { rtid } = res1.rows[i];
+                const res2 = await client.query(paymentQueryText, [rtid]);
+                const { status } = res2.rows[0];
+                res1.rows[i].status = status;
+            }
             res.status(200).jsonp({
-                rental_agreement_list: rows
+                rental_agreement_list: res1.rows
             });
         } catch (err) {
             console.error('Error during the query.', err.stack);
@@ -32,17 +40,23 @@ const handleHostRentalList = async (req, res, db_pool) => {
 const handleGuestRentalList = async (req, res, db_pool) => {
     // get http path parameter
     const { gid } = req.params;
-    // console.log(gid); // test
 
     try {
         const client = await db_pool.connect();
         try {
             const queryText =
                 'SELECT * FROM project.rental_agreement WHERE gid = $1;';
-            const { rows } = await client.query(queryText, [gid]);
-            // console.log(rows); // test
+            const res1 = await client.query(queryText, [gid]);
+            const paymentQueryText =
+                'SELECT status FROM project.payment WHERE rtid = $1;';
+            for (i in res1.rows) {
+                const { rtid } = res1.rows[i];
+                const res2 = await client.query(paymentQueryText, [rtid]);
+                const { status } = res2.rows[0];
+                res1.rows[i].status = status;
+            }
             res.status(200).jsonp({
-                rental_agreement_list: rows
+                rental_agreement_list: res1.rows
             });
         } catch (err) {
             console.error('Error during the query.', err.stack);
