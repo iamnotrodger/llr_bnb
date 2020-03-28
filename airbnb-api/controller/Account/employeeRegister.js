f602facbe35f8949883cfb651a04eb8719a9bfed
cyclexit <cyclexit@gmail.com>
2020-03-27 22:47:03 -0400

    Update employee register: add country to usr table instead of employee table; make sure there is one manager for one branch

--- airbnb-api/controller/Account/employeeRegister.js
+++ airbnb-api/controller/Account/employeeRegister.js
@@ -29,6 +29,10 @@ const handleRegister = async (req, res, db_pool, Joi, CryptoJS) => {
             .required(),
         position: Joi.string()
             .max(30)
+            .valid([
+                'manager',
+                'employee'
+            ])
             .required(),
         salary: Joi.number()
             .required()
@@ -59,35 +63,49 @@ const handleRegister = async (req, res, db_pool, Joi, CryptoJS) => {
         const client = await db_pool.connect();
         try {
             await client.query('BEGIN');
-            // insert into user table
-            const userText =
-                'INSERT INTO project.usr(firstName, middleName, lastName, email, address, phoneNum) VALUES($1, $2, $3, $4, $5, $6) RETURNING uid;';
-            const res1 = await client.query(userText, [
-                firstName,
-                middleName,
-                lastName,
-                email,
-                address,
-                phoneNum
-            ]);
-            const { uid } = res1.rows[0];
-            // insert into employee table
-            const employeeText = 
-                'INSERT INTO project.employee(uid, country, salary, position) VALUES($1, $2, $3, $4) RETURNING empid;';
-            const res2 = await client.query(employeeText, [uid, country, salary, position]);
-            const { empid } = res2.rows[0];
-            // insert into login table
-            const loginText =
-                'INSERT INTO project.login(email, password) VALUES($1, $2);';
-            await client.query(loginText, [
-                email,
-                words.toString() // encrypted password
-            ]);
-            await client.query('COMMIT');
-            res.status(200).jsonp({
-                uid: uid,
-                empid: empid
-            });
+            // check if manager exists
+            if (position == 'manager') {
+                const managerText = 
+                    'SELECT COUNT(*) AS manager_num FROM project.employee NATURAL JOIN project.usr WHERE country = $1 AND position = \'manager\' GROUP BY country;';
+                const res2 = await client.query(managerText, [country]);
+                // console.log(res2.rows.length); // test
+                if (res2.rows.length != 0) {
+                    // this means that there is a manger in this country already
+                    res.status(400).json('There is a manager in this country already');
+                    return;
+                }
+            } else {
+                // insert into user table
+                const userText =
+                    'INSERT INTO project.usr(firstName, middleName, lastName, email, address, phoneNum, country) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING uid;';
+                const res1 = await client.query(userText, [
+                    firstName,
+                    middleName,
+                    lastName,
+                    email,
+                    address,
+                    phoneNum,
+                    country
+                ]);
+                const { uid } = res1.rows[0];
+                // insert into employee table
+                const employeeText = 
+                    'INSERT INTO project.employee(uid, salary, position) VALUES($1, $2, $3) RETURNING empid;';
+                const res2 = await client.query(employeeText, [uid, salary, position]);
+                const { empid } = res2.rows[0];
+                // insert into login table
+                const loginText =
+                    'INSERT INTO project.login(email, password) VALUES($1, $2);';
+                await client.query(loginText, [
+                    email,
+                    words.toString() // encrypted password
+                ]);
+                await client.query('COMMIT');
+                res.status(200).jsonp({
+                    uid: uid,
+                    empid: empid
+                });
+            }
         } catch (err) {
             console.error(
                 'Error during the transaction, ROLLBACK.',
